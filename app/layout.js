7c994958fac50bda93f2692801abe4ec82c3345e
Mohamed esmail <dev.mohamed.esmail@gmail.com>
2025-05-07 14:28:28 +0400

    Make Multi Lag

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import I18nProvider from "./components/I18nProvider/I18nProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,10 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <I18nProvider>
+          {children}
+        </I18nProvider>
+        
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from "next/image";
+import LanguageSwitcher from "./components/LanguageSwitcher/LanguageSwitcher";
 
 export default function Home() {
   return (
     <div >
      
-     
+     <LanguageSwitcher />
     </div>
   );
 }
