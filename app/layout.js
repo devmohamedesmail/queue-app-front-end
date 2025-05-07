3a2baab4dd9df54fd0de132fcac5b74c92646c44
Mohamed esmail <dev.mohamed.esmail@gmail.com>
2025-05-07 17:14:20 +0400

    Remove Language

--- app/layout.js
+++ app/layout.js
@@ -2,15 +2,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata = {
   title: "Create Next App",
@@ -18,13 +11,16 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` antialiased`}
       >
       
+          
           {children}
+      
         
       </body>
     </html>
--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,7 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  };
