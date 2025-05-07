bfcbe09480d0f4cfdf6b922428033c4ed564b3c2
Mohamed esmail <dev.mohamed.esmail@gmail.com>
2025-05-07 18:28:38 +0400

    Create Banner

---
+++ app/components/Banner.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Banner() {
+    return (
+        <div className='container m-auto px-5' >
+
+            <img src="/images/banner.jpg" alt="" className='w-full h-60' />
+        </div>
+
+    )
+}
--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Map from "./components/Map";
 import Place_section from "./sections/Place_section";
 import Mobile_Dock from "./components/Mobile_Dock";
 import Footer from "./components/Footer";
+import Banner from "./components/Banner";
 
 
 
@@ -11,6 +12,7 @@ export default function Home() {
     <div >
      <Navbar />
      <Map />
+     <Banner />
      <Place_section />
      <Mobile_Dock />
      <Footer />
