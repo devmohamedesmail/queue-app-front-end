627f08b8c0872e354b1f51b59bb7c892ec719279
Mohamed esmail <dev.mohamed.esmail@gmail.com>
2025-05-08 13:00:13 +0400

    update

--- app/custom/Custom_service_item.jsx
+++ app/custom/Custom_service_item.jsx
@@ -1,10 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
 
-export default function Custom_service_item({service}) {
-  return (
-    <div className='p-2 border border-primary flex flex-col justify-center items-center py-10 rounded-xl' >
-        <p className='font-bold'>{service.nameEn}</p>
-        <p className='font-bold'>{service.nameAr}</p>
-    </div>
-  )
+export default function Custom_service_item({ service }) {
+    return (
+        <div className='p-2 border border-primary  py-10 rounded-xl'>
+
+            <div className=' flex flex-col justify-center items-center ' >
+                <p className='font-bold'>{service.nameEn}</p>
+                <p className='font-bold'>{service.nameAr}</p>
+
+            </div>
+            <div className='flex justify-between items-center'>
+                <p>clients </p>
+                <p>100</p>
+            </div>
+            <div className='flex justify-between items-center'>
+                <p>Estimating Time </p>
+                <p>100</p>
+                <p>{service.placeId}</p>
+            </div>
+            <Link href={{
+                pathname: '/pages/user/waiting',
+                query: { placeId: service.placeId, serviceId: service._id }
+            }}>Book Queue</Link>
+        </div>
+    )
 }
---
+++ app/pages/user/waiting/page.jsx
@@ -0,0 +1,41 @@
+import Footer from '@/app/components/Footer'
+import Mobile_Dock from '@/app/components/Mobile_Dock'
+import Navbar from '@/app/components/Navbar'
+import React from 'react'
+
+export default function page({ searchParams }) {
+  console.log("searchParams", searchParams.placeId)
+  return (
+    <div>
+      <Navbar />
+
+
+      <div className='container m-auto px-5 flex flex-col justify-center items-center gap-5 py-10'>
+
+        <div className='card w-full bg-base-100 shadow-xl p-3'>
+          <div className='flex flex-col justify-between items-center'>
+            <h1 className='text-primary font-bold text-3xl'>Waiting</h1>
+            <h1>10</h1>
+          </div>
+
+          <div className='flex justify-between items-center my-10'>
+            <p>Estaimating</p>
+            <p>1000</p>
+          </div>
+          <div className='flex justify-center items-center my-10'>
+            <button className='btn btn-neutral w-full'>Book now</button>
+          </div>
+        </div>
+
+
+
+      </div>
+
+
+
+      <Footer />
+      <Mobile_Dock />
+
+    </div>
+  )
+}
