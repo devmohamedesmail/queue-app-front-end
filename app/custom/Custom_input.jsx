5111b5a879dc57c7ad5ec05c22ce07bf7b043567
Mohamed esmail <dev.mohamed.esmail@gmail.com>
2025-05-10 15:03:13 +0400

    redesign login page

--- app/custom/Custom_input.jsx
+++ app/custom/Custom_input.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Custom_input({ label, type, placeholder, value, onChange }) {
   return (
     <div className='mb-5 w-full'>
-        <label className='text-sm'>{label}</label>
+        <label className='text-xs mb-1  w-full block text-left'>{label}</label>
         <input type={type} placeholder={placeholder} value={value} onChange={onChange} className="input input-neutral focus:outline-0 w-full border-gray-400 h-12" />
     </div>
   )
--- app/pages/auth/login/page.jsx
+++ app/pages/auth/login/page.jsx
@@ -10,10 +10,12 @@ import Link from 'next/link'
 import React, { useContext, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
 export default function page() {
+      const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [loading,setLoading] = useState(false)
-    const {auth, setAuth, login, register, logout}=useContext(AuthContext)
+    const [loading, setLoading] = useState(false)
+    const { auth, setAuth, login, register, logout } = useContext(AuthContext)
+    const [activeTab, setActiveTab] = useState('login')
 
 
 
@@ -21,7 +23,7 @@ export default function page() {
     const handle_login_user = async () => {
         try {
             setLoading(true)
-            const res = await login( email, password)
+            const res = await login(email, password)
             console.log(res)
             if (res.status === 200) {
                 toast.success('✅ Registration successful');
@@ -35,6 +37,31 @@ export default function page() {
     }
 
 
+      const handle_register_user = async (e) => {
+    
+        try {
+          setLoading(true)
+          const res = await register(name, email, password)
+          if (res.status === 201) {
+            toast.success('✅ Registration successful');
+    
+            setName('')
+            setEmail('')
+            setPassword('')
+          }
+          setLoading(false)
+    
+        } catch (error) {
+          setLoading(false)
+    
+        } finally {
+          setLoading(false)
+        }
+    
+    
+      }
+    
+
 
 
 
@@ -43,19 +70,47 @@ export default function page() {
         <div>
             <Navbar />
             <div className='container m-auto px-5'>
-                <div className='flex flex-col justify-center h-screen  items-center   w-full md:w-96 mx-auto  '>
-                    <h1 className='text-3xl font-bold text-center mb-3'>Login</h1>
-                    <p className='text-center'>Welcome back! Please login to your account.</p>
 
-                    <Custom_input type={"email"} value={email} onChange={(e) => setEmail(e.target.value)} label={"Email"} />
-                    <Custom_input type={"password"} value={password} onChange={(e) => setPassword(e.target.value)} label={"Password"} />
-                
-                    {loading ? <Custom_spinner /> : <Custom_button title="Login" onClick={handle_login_user} />}
-                    <div className='my-5'>
-                        <p className='text-center'>Don't have an account? <Link href="/pages/auth/register/" className='text-blue-500'>Register</Link></p>
+
+
+                <div className='w-full md:1/2 lg:w-1/3 m-auto mt-10 p-5  shadow-lg rounded-lg'>
+                    <div className='flex justify-center gap-5 my-10 bg-gray-200 py-2 px-2 rounded-lg'>
+                        <button className={`px-5 py-2 flex-1 transition-colors ease-in-out duration-500 rounded-lg ${activeTab === 'login' ? 'bg-black text-white' : 'bg-white text-black'}`} onClick={() => setActiveTab('login')}>Login</button>
+                        <button className={`px-5 py-2 flex-1 transition-colors ease-in-out duration-500 rounded-lg ${activeTab === 'register' ? 'bg-black text-white' : 'bg-white text-black'}`} onClick={() => setActiveTab('register')}>register</button>
+                    </div>
+
+                    <div className="text-center text-2xl font-semibold">
+                        {activeTab === 'login' &&
+                            <div className='  '>
+                                <h1 className='text-xl font-bold text-center mb-3'>Login</h1>
+                                <Custom_input type={"email"} value={email} onChange={(e) => setEmail(e.target.value)} label={"Email"} />
+                                <Custom_input type={"password"} value={password} onChange={(e) => setPassword(e.target.value)} label={"Password"} />
+
+                                {loading ? <Custom_spinner /> : <Custom_button title="Login" onClick={handle_login_user} />}
+                               
+                            </div>}
+                        {activeTab === 'register' && 
+                        
+                        <div>
+                                  <h1 className='text-xl font-bold text-center mb-3'>Register</h1>
+                                  <Custom_input type={"text"} value={name} onChange={(e) => setName(e.target.value)} label={"Name"} />
+                                  <Custom_input type={"email"} value={email} onChange={(e) => setEmail(e.target.value)} label={"Email"} />
+                                  <Custom_input type={"password"} value={password} onChange={(e) => setPassword(e.target.value)} label={"Password"} />
+                        
+                        
+                                  {loading ? <span className="loading loading-spinner loading-xs"></span> : <Custom_button title="Register" onClick={handle_register_user} />}    
+                        </div>}
                     </div>
 
                 </div>
+
+
+
+
+
+
+
+
             </div>
             <Footer />
             <Mobile_Dock />
