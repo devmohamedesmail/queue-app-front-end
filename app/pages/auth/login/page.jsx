6a996ce1445c867f45d92c3fec021d07b62b3119
Mohamed esmail <dev.mohamed.esmail@gmail.com>
2025-05-12 16:48:14 +0400

    make Login and redirect

--- app/pages/auth/login/page.jsx
+++ app/pages/auth/login/page.jsx
@@ -9,111 +9,249 @@ import Custom_spinner from '@/app/custom/Custom_spinner'
 import Link from 'next/link'
 import React, { useContext, useState } from 'react'
 import { ToastContainer, toast } from 'react-toastify';
+import { useTranslation } from 'react-i18next'
+import { Formik, Field, Form, ErrorMessage, useFormik } from 'formik'
+import * as Yup from 'yup'
+import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
+
 export default function page() {
-      const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [loading, setLoading] = useState(false)
-    const { auth, setAuth, login, register, logout } = useContext(AuthContext)
-    const [activeTab, setActiveTab] = useState('login')
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const { auth, setAuth, login, register, logout } = useContext(AuthContext)
+  const [activeTab, setActiveTab] = useState('login')
+  const { t, i18n } = useTranslation();
+const router = useRouter()
 
 
 
 
-    const handle_login_user = async () => {
-        try {
-            setLoading(true)
-            const res = await login(email, password)
-            console.log(res)
-            if (res.status === 200) {
-                toast.success('✅ Registration successful');
-                setEmail('')
-                setPassword('')
-            }
-            setLoading(false)
-        } catch (error) {
-            console.log(error)
+  const loginFormik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    validationSchema: Yup.object({
+      email: Yup.string().email(t('invalid-email')).required(t('required')),
+      password: Yup.string().required(t('required')),
+    }),
+    onSubmit: async (values) => {
+      try {
+        setLoading(true)
+        const res = await login(values.email, values.password)
+        console.log(res.user.user.user.role);
+        if (res.status === 200) {
+          toast.success(t('success'));
+          setEmail('')
+          setPassword('')
         }
+
+        const role = res.user.user.user.role
+        if (role === 'admin') {
+         
+          router.push('/pages/admin')
+        } else if (role === 'subscriber') {
+          
+          router.push('/pages/subscriber')
+        } else {
+          redirect('/pages/user')
+          router.push('/pages/user')
+        }
+        setLoading(false)
+      } catch (err) {
+        toast.log(t('login-failed'));
+      } finally {
+        setLoading(false);
+      }
+    },
+  });
+
+
+
+
+
+
+  const registerFormik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: '',
+
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required(t('required')),
+      email: Yup.string().email(t('invalid-email')).required(t('required')),
+      password: Yup.string().min(6, t('min-6')).required(t('required')),
+
+    }),
+    onSubmit: async (values) => {
+      try {
+        setLoading(true);
+        const res = await register(values.name, values.email, values.password)
+        if (res.status === 201) {
+          toast.success('✅ Registration successful');
+          setName('')
+          setEmail('')
+          setPassword('')
+        }
+
+        setLoading(false)
+      } catch (err) {
+        toast.error(t('register-failed'));
+      } finally {
+        setLoading(false);
+      }
+    },
+  });
+
+
+
+
+
+
+
+  const handle_login_user = async () => {
+    try {
+      setLoading(true)
+      const res = await login(email, password)
+      console.log(res)
+      if (res.status === 200) {
+        toast.success('✅ Registration successful');
+        setEmail('')
+        setPassword('')
+      }
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+  const handle_register_user = async (e) => {
+
+    try {
+      setLoading(true)
+      const res = await register(name, email, password)
+      if (res.status === 201) {
+        toast.success('✅ Registration successful');
+
+        setName('')
+        setEmail('')
+        setPassword('')
+      }
+      setLoading(false)
+
+    } catch (error) {
+      setLoading(false)
+
+    } finally {
+      setLoading(false)
     }
 
 
-      const handle_register_user = async (e) => {
-    
-        try {
-          setLoading(true)
-          const res = await register(name, email, password)
-          if (res.status === 201) {
-            toast.success('✅ Registration successful');
-    
-            setName('')
-            setEmail('')
-            setPassword('')
-          }
-          setLoading(false)
-    
-        } catch (error) {
-          setLoading(false)
-    
-        } finally {
-          setLoading(false)
-        }
-    
-    
-      }
-    
-
-
-
-
-
-    return (
-        <div>
-            <Navbar />
-            <div className='container m-auto px-5'>
-
-
-
-                <div className='w-full md:1/2 lg:w-1/3 m-auto mt-10 p-5  shadow-lg rounded-lg'>
-                    <div className='flex justify-center gap-5 my-10 bg-gray-200 py-2 px-2 rounded-lg'>
-                        <button className={`px-5 py-2 flex-1 transition-colors ease-in-out duration-500 rounded-lg ${activeTab === 'login' ? 'bg-black text-white' : 'bg-white text-black'}`} onClick={() => setActiveTab('login')}>Login</button>
-                        <button className={`px-5 py-2 flex-1 transition-colors ease-in-out duration-500 rounded-lg ${activeTab === 'register' ? 'bg-black text-white' : 'bg-white text-black'}`} onClick={() => setActiveTab('register')}>register</button>
-                    </div>
-
-                    <div className="text-center text-2xl font-semibold">
-                        {activeTab === 'login' &&
-                            <div className='  '>
-                                <h1 className='text-xl font-bold text-center mb-3'>Login</h1>
-                                <Custom_input type={"email"} value={email} onChange={(e) => setEmail(e.target.value)} label={"Email"} />
-                                <Custom_input type={"password"} value={password} onChange={(e) => setPassword(e.target.value)} label={"Password"} />
-
-                                {loading ? <Custom_spinner /> : <Custom_button title="Login" onClick={handle_login_user} />}
-                               
-                            </div>}
-                        {activeTab === 'register' && 
-                        
-                        <div>
-                                  <h1 className='text-xl font-bold text-center mb-3'>Register</h1>
-                                  <Custom_input type={"text"} value={name} onChange={(e) => setName(e.target.value)} label={"Name"} />
-                                  <Custom_input type={"email"} value={email} onChange={(e) => setEmail(e.target.value)} label={"Email"} />
-                                  <Custom_input type={"password"} value={password} onChange={(e) => setPassword(e.target.value)} label={"Password"} />
-                        
-                        
-                                  {loading ? <span className="loading loading-spinner loading-xs"></span> : <Custom_button title="Register" onClick={handle_register_user} />}    
-                        </div>}
-                    </div>
-
-                </div>
+  }
 
 
 
 
 
 
+  return (
+    <div>
+      <Navbar />
+      <div className='container m-auto px-5'>
 
 
-            </div>
-            <Footer />
-            <Mobile_Dock />
+
+        <div className='w-full md:1/2 lg:w-1/3 m-auto mt-10 p-5  shadow-lg rounded-lg'>
+          <div className='flex justify-center gap-5 my-10 bg-gray-200 py-2 px-2 rounded-lg'>
+            <button className={`px-5 py-2 flex-1 transition-colors ease-in-out duration-500 rounded-lg ${activeTab === 'login' ? 'bg-black text-white' : 'bg-white text-black'}`} onClick={() => setActiveTab('login')}>{t('login')}</button>
+            <button className={`px-5 py-2 flex-1 transition-colors ease-in-out duration-500 rounded-lg ${activeTab === 'register' ? 'bg-black text-white' : 'bg-white text-black'}`} onClick={() => setActiveTab('register')}>{t('register')}</button>
+          </div>
+
+          <div className="text-center text-2xl font-semibold">
+            {activeTab === 'login' &&
+              (
+                <form onSubmit={loginFormik.handleSubmit}>
+                  <div className='  '>
+                    <h1 className='text-xl font-bold text-center mb-3'>{t('login')}</h1>
+                    <Custom_input
+                      type={"email"}
+                      name="email"
+                      value={loginFormik.values.email}
+                      onChange={loginFormik.handleChange}
+                      label={t('email')}
+                      error={loginFormik.touched.email && loginFormik.errors.email}
+                    />
+                    <Custom_input
+                      type={"password"}
+                      name="password"
+                      value={loginFormik.values.password}
+                      onChange={loginFormik.handleChange}
+                      label={t('password')}
+                      error={loginFormik.touched.password && loginFormik.errors.password}
+                    />
+
+                    {loading ? <Custom_spinner /> : <Custom_button type='submit' title={t('login')} />}
+
+                  </div>
+                </form>
+              )
+
+            }
+            {activeTab === 'register' &&
+
+              (
+                <form onSubmit={registerFormik.handleSubmit}>
+                  <div>
+                    <h1 className='text-xl font-bold text-center mb-3'>{t('register')}</h1>
+                    <Custom_input
+                      type={"text"}
+                      name="name"
+                      value={registerFormik.values.name}
+                      onChange={registerFormik.handleChange} label={t('name')}
+                      error={registerFormik.touched.name && registerFormik.errors.name}
+                    />
+                    <Custom_input
+                      type={"email"}
+                      name="email"
+                      value={registerFormik.values.email}
+                      onChange={registerFormik.handleChange}
+                      label={t('email')}
+                      error={registerFormik.touched.email && registerFormik.errors.email}
+                    />
+                    <Custom_input
+                      type={"password"}
+                      name="password"
+                      value={registerFormik.values.password} onChange={registerFormik.handleChange}
+                      label={t('password')}
+                      error={registerFormik.touched.password && registerFormik.errors.password}
+                    />
+
+
+                    {loading ? <span className="loading loading-spinner loading-xs"></span> : <Custom_button title={t('register')} type='submit' />}
+                  </div>
+                </form>
+              )
+
+
+            }
+          </div>
+
         </div>
-    )
+
+
+
+
+
+
+
+
+      </div>
+      <Footer />
+      <Mobile_Dock />
+    </div>
+  )
 }
